a79bd1e998a6a80e1b964855625bd5c48ef014bb
uszakow <51965778+uszakow@users.noreply.github.com>
2019-11-26 23:00:38 +0100

    Start commit

---
+++ main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// const nameOfPersonLabel = document.getElementById('nameOfPersonLabel');
+// nameOfPersonLabel.style.display = 'none';
+
+
+export { document };
---
+++ src/controller.js
@@ -0,0 +1,13 @@
+'use strict';
+import {document} from '../main';
+
+
+const typeOfEvent = document.getElementById('typeOfEvent');
+
+const nameOfPersonLabel = document.getElementById('nameOfPersonLabel');
+const topicOfMeetingLabel = document.getElementById('topicOfMeetingLabel');
+const shoppingListLabel = document.getElementById('shoppingListLabel');
+const titleOfLessonLabel = document.getElementById('titleOfLessonLabel');
+const nameOfTeacherLabel = document.getElementById('nameOfTeacherLabel');
+const otherEventLabel = document.getElementById('otherEventLabel');
+nameOfPersonLabel.style.display = 'none';
